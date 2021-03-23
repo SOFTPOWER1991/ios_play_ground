0f2e658cf043b060afc983dd17fdf1a5bdd9c8b0
zhanggeng <zhanggengdyx@gmail.com>
2021-03-23 22:49:59 +0800

    UITableView

---
+++ Doc/13-UITableView.md
@@ -0,0 +1,8 @@
+#  UITableView
+
+类似于Android中的RecylerView 和 ListView，类似于Flutter中的ListView
+
+UITableView = tableHeaderView + UITableViewCell + tableFooterView
+
+
+
--- PlayGround/ViewController.m
+++ PlayGround/ViewController.m
@@ -16,31 +16,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    UILabel *label = [[UILabel alloc] init];
-    label.text = @"Go to Next Page";
-    label.userInteractionEnabled = true;
-    [label sizeToFit];
-    label.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2);
+//    UILabel *label = [[UILabel alloc] init];
+//    label.text = @"Go to Next Page";
+//    label.userInteractionEnabled = true;
+//    [label sizeToFit];
+//    label.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2);
+//
+//    self.view.backgroundColor = [UIColor whiteColor];
+//    [self.view addSubview:({
+//            label;
+//        })];
+//
+//    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(pushController)];
+//    [label addGestureRecognizer:tapGesture];
+//
     
-    self.view.backgroundColor = [UIColor whiteColor];
-    [self.view addSubview:({
-            label;
-        })];
-    
-    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(pushController)];
-    [label addGestureRecognizer:tapGesture];
-    
+    UITableView *uiTableView = [[UITableView alloc] initWithFrame:self.view.bounds];
+    [self.view addSubview:uiTableView];
 }
 
--(void)pushController{
-    
-    UIViewController *viewController = [[UIViewController alloc]init];
-    viewController.view.backgroundColor = [UIColor whiteColor];
-    viewController.navigationItem.title = @"next page";
-    
-    viewController.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"右侧标题"  style:UIBarButtonItemStylePlain target:self action:nil];
-    
-    [self.navigationController pushViewController:[[UIViewController alloc]init] animated:true];
-}
+
+//-(void)pushController{
+//
+//    UIViewController *viewController = [[UIViewController alloc]init];
+//    viewController.view.backgroundColor = [UIColor whiteColor];
+//    viewController.navigationItem.title = @"next page";
+//
+//    viewController.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"右侧标题"  style:UIBarButtonItemStylePlain target:self action:nil];
+//
+//    [self.navigationController pushViewController:[[UIViewController alloc]init] animated:true];
+//}
 
 @end
